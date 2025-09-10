9178236f7129ea86a64acd923aa49685ab8945b5
saradamm1 <saradamm98@gmail.com>
2025-09-10 00:50:55 -0300

    bancos

--- consumer.py
+++ consumer.py
@@ -1,11 +1,41 @@
 from kafka import KafkaConsumer
 import json
+import mysql.connector
+from pymongo import MongoClient
+
+# === Configurações MySQL ===
+mysql_conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="190600sara",
+    database="restaurante"
+)
+mysql_cursor = mysql_conn.cursor()
+
+mongo_client = MongoClient("mongodb://localhost:27017/")
+mongo_db = mongo_client["restaurante"]
+mongo_collection = mongo_db["pedidos"]
 
 consumer = KafkaConsumer(
     'pedidos',
     bootstrap_servers='localhost:9092',
     value_deserializer=lambda m: json.loads(m.decode('utf-8'))
 )
-print("esperando pedidos")
+
+print("Esperando pedidos...")
 for msg in consumer:
-    print("Novo pedido recebido:", msg.value)
+    pedido = msg.value
+    print("Novo pedido recebido:", pedido)
+
+    # --- Inserir cliente se não existir (só id) ---
+    cliente_id = pedido["cliente"]["id"]
+    mysql_cursor.execute(
+        "INSERT INTO pedido (id_cliente) VALUES (%s)",
+        (cliente_id,)
+    )
+    mysql_conn.commit()
+
+    # --- Salvar pedido completo no MongoDB ---
+    mongo_collection.insert_one(pedido)
+
+    print("Pedido salvo no MySQL e MongoDB!")
--- producer.py
+++ producer.py
@@ -6,7 +6,7 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-pedido = {"pedido_id": 1, "cliente": "Bela", "valor": 59.9, "status": "criado"}
+pedido = {"pedido_id": 1, "cliente": {"id":"1","nome":"Bela"}, "valor": 59.9, "status": "criado"}
 
 producer.send('pedidos', pedido)
 producer.flush()
